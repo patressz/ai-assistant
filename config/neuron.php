fcbc9bd8a89ab493f214f217c87b90ea389c984d
Patrik Strišovský <patrik.strisovsky7@gmail.com>
2026-05-07 19:22:41 +0200

    style: adjust formatting

--- config/neuron.php
+++ config/neuron.php
@@ -117,7 +117,7 @@ return [
             'key' => env('ELEVENLABS_KEY'),
             'model' => env('ELEVENLABS_STT_MODEL', 'elevenlabs/stt-v1'),
             'parameters' => [],
-        ]
+        ],
     ],
 
     /*
@@ -164,7 +164,7 @@ return [
             'key' => env('MISTRAL_KEY'),
             'model' => env('MISTRAL_EMBEDDING_MODEL', 'mistral-7b-embed-v1'),
             'dimensions' => 1024,
-        ]
+        ],
     ],
 
     /*
